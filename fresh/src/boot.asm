c6e46aa7f65dfa35ec50d5a8ae57419e422f3015
carson <carson42g@gmail.com>
2019-12-20 14:50:54 -0500

    messages

--- fresh/src/boot.asm
+++ fresh/src/boot.asm
@@ -1,19 +1,25 @@
 [bits 16]
 [org 0x7c00]
-  mov bx, HELLO_WORLD
+  mov bx, STARTED_IN_16_BIT_MODE
   call print_string
+  
   mov ax, 0xFFFF
-  ;call print_ax_bin
   call print_ax_hex
+
   mov bx, NEWLINE
+  call print_string
+
+  mov bx, BOOT_CODE_TERMINATED
   call print_string
   jmp $
 
 %include "src/printer_16bit.asm"
 
-HELLO_WORLD db "Hello, World!", 10, 13, 0
+STARTED_IN_16_BIT_MODE db "Started in 16 bit mode", 10, 13, 0
+BOOT_CODE_TERMINATED db "Boot code has terminated", 10, 13, 0
 NEWLINE db 10, 13, 0
-ZERO db "[zero]", 0
+
+
 times 510-($-$$) db 0
 
 dw 0xAA55
